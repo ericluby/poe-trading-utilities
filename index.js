e26636d3052e52e2d79ae7e0a277bf30b7989f05
ericluby <60050306+ericluby@users.noreply.github.com>
2020-01-22 20:23:06 -0500

    two methods of fetch

--- index.js
+++ index.js
@@ -5,50 +5,41 @@ const fetch = require('node-fetch');
 const cardURL = 'https://poe.ninja/challenge/divinationcards/the-polymath'
 const itemURL = 'https://poe.ninja/challenge/unique-accessories/astramentis-onyx-amulet'
 
-// fetch("https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en", {
-//   "credentials": "omit",
-//   "referrer": "https://poe.ninja/challenge/unique-accessories/astramentis-onyx-amulet",
-//   "referrerPolicy": "no-referrer-when-downgrade",
-//   "body": null,
-//   "method": "GET",
-//   "mode": "cors"
-// });
-//the item
-fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en')
-  .then((response) => {
-    return response.json();
-  })
-  .then((responseJson) => {
-    //single out item
-    const resultJson = responseJson.lines.filter(item => item.name === 'Astramentis')[0];
-    const item = {
-      name: item.name,
-      stackSize: item.stackSize,
-      chaos: item.chaosValue
-    }
-    console.log(item)
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-//the div card
-fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=DivinationCard&language=en')
-  .then((response) => {
-    return response.json();
-  })
-  .then((responseJson) => {
-    //single out item
-    const resultJson = responseJson.lines.filter(item => item.name === 'The Polymath')[0];
-    const divCard = {
-      name: item.name,
-      stackSize: item.stackSize,
-      chaos: item.chaosValue
-    }
-    console.log(item)
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+//main function
+async function main(){
+  //fetch the item
+  const response = await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en')
+  const responseJson = await response.json();
+  //single out item
+  const resultJson = responseJson.lines.filter(item => item.name === 'Astramentis')[0];
+  const combinedItem = {
+    name: resultJson.name,
+    stackSize: resultJson.stackSize,
+    chaos: resultJson.chaosValue
+  }
+  console.log(combinedItem)
+
+  //fetch the div card
+  fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=DivinationCard&language=en')
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJson) => {
+      //single out div card
+      const resultJson = responseJson.lines.filter(item => item.name === 'The Polymath')[0];
+      const divCard = {
+        name: resultJson.name,
+        stackSize: resultJson.stackSize,
+        chaos: resultJson.chaosValue
+      }
+      console.log(divCard)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+main();
+
 //div card stack price
   //div card stacksize * div card chaosValue
 //profit margin
