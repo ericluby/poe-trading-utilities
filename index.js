cf023d43db37e6bbdfda9e47b5d11c897a9d5b5c
ericluby <60050306+ericluby@users.noreply.github.com>
2020-01-23 18:20:32 -0500

    searches for all div accessory pairs

--- index.js
+++ index.js
@@ -4,33 +4,48 @@ async function main(){
   // fetch the div card
   const divCardResponseJson = await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=DivinationCard&language=en').then(r => r.json());
   // single out div card
-  const divCardJson = divCardResponseJson.lines.filter(item => item.name === 'The Polymath')[0];
-  const divCard = {
-    name: divCardJson.name,
-    stackSize: divCardJson.stackSize,
-    chaos: divCardJson.chaosValue,
-    explicitModifiers: divCardJson.explicitModifiers
-  };
-//
-  let divString = divCard.explicitModifiers[0].text;
-  let divArray = divString.split('}')[0].split('{');
-  console.log(divArray[0]);
-  console.log(divArray[1]);
-//
-  console.log(divCard);
+  //const divCardJson = divCardResponseJson.lines.filter(item => item.name === 'The Polymath')[0];
+  const divCardJson = divCardResponseJson.lines;
+  const uniqueDivCards = divCardJson.map(function(item){
+    if (item.explicitModifiers[0]) {
+      // return item.explicitModifiers[0].text
+      const divCard = {
+        name: item.name,
+        stackSize: item.stackSize,
+        chaos: item.chaosValue,
+        explicitModifiers: item.explicitModifiers[0].text
+      };
+      let divString = divCard.explicitModifiers;
+      let divArray = divString.split('}')[0].split('{'); //index 0 = <uniqueitem>, index 1 = item name
+      if (divArray[0] === '<uniqueitem>'){
+        divCard.uniqueItem = divArray[1];
+        return divCard;
+      } else{
+        return null
+      }
+    } else {
+      return null
+    }
+  }).filter(Boolean);
+  //console.log('uniqueDivCards' , uniqueDivCards);
+  //
   // fetch the item
-  const combinedItemResponseJson = await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en').then(r => r.json());
+  const uniqueAccessories = (await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en').then(r => r.json())).lines;
   // single out item
-  const combinedItemJson = combinedItemResponseJson.lines.filter(item => item.name === divArray[1])[0];
-  const combinedItem = {
-    name: combinedItemJson.name,
-    stackSize: combinedItemJson.stackSize,
-    chaos: combinedItemJson.chaosValue
-  };
-  console.log(combinedItem);
-  // calculating profit margins
-  const divCardStackPrice = divCard.stackSize * divCard.chaos;
-  const profitMargin = combinedItem.chaos - divCardStackPrice;
-  console.log(`By flipping ${divCard.name} into ${combinedItem.name} you can make ${profitMargin} chaos.`);
+  const uniqueAccessoryJson = uniqueAccessories.forEach(function (uniqueAccessory) {
+    const combinedItem = {
+      name: uniqueAccessory.name,
+      stackSize: uniqueAccessory.stackSize,
+      chaos: uniqueAccessory.chaosValue
+    };
+    const matchingUniqueDivCard = uniqueDivCards.find(divCard => divCard.uniqueItem === combinedItem.name);
+    if (!matchingUniqueDivCard){
+      return;
+    }
+    // calculating profit margins
+    const divCardStackPrice = matchingUniqueDivCard.stackSize * matchingUniqueDivCard.chaos;
+    const profitMargin = combinedItem.chaos - divCardStackPrice;
+    console.log(`By flipping ${matchingUniqueDivCard.name} into ${combinedItem.name} you can make ${profitMargin} chaos.`);
+  });
 };
 main();
