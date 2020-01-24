0348aae334f17b9f19c6d9abbeb3f547f33d161c
ericluby <60050306+ericluby@users.noreply.github.com>
2020-01-24 17:59:04 -0500

    add server

--- getDivCardsAndItems.js
+++ getDivCardsAndItems.js
@@ -1,10 +1,61 @@
-const fetch = require('node-fetch');
+if (typeof window === 'undefined'){
+  var fetch = require('node-fetch');
+}; //else we are in a browser
 // main function
-async function main(){
+async function getDivCardsAndItems(){
+  return [
+    {
+      "matchingUniqueDivCard": {
+        "name": "The Mayor",
+        "stackSize": 5,
+        "chaos": 19,
+        "explicitModifiers": "<uniqueitem>{The Perandus Manor}",
+        "uniqueItem": "The Perandus Manor"
+      },
+      "combinedItem": {
+        "name": "The Perandus Manor",
+        "stackSize": 0,
+        "chaos": 115
+      },
+      "divCardStackPrice": 95,
+      "profitMargin": 20
+    },
+    {
+      "matchingUniqueDivCard": {
+        "name": "The Professor",
+        "stackSize": 4,
+        "chaos": 6,
+        "explicitModifiers": "<uniqueitem>{The Putrid Cloister}",
+        "uniqueItem": "The Putrid Cloister"
+      },
+      "combinedItem": {
+        "name": "The Putrid Cloister",
+        "stackSize": 0,
+        "chaos": 60
+      },
+      "divCardStackPrice": 24,
+      "profitMargin": 36
+    },
+    {
+      "matchingUniqueDivCard": {
+        "name": "The Landing",
+        "stackSize": 5,
+        "chaos": 8.87,
+        "explicitModifiers": "<uniqueitem>{The Beachhead}\n<default>{Map Tier:} <normal>{15}\n<corrupted>{Corrupted}",
+        "uniqueItem": "The Beachhead"
+      },
+      "combinedItem": {
+        "name": "The Beachhead",
+        "stackSize": 0,
+        "chaos": 56
+      },
+      "divCardStackPrice": 44.349999999999994,
+      "profitMargin": 11.650000000000006
+    }
+  ] // sample data to delete later
   // fetch the div card
   const divCardResponseJson = await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=DivinationCard&language=en').then(r => r.json());
   // single out div card
-  //const divCardJson = divCardResponseJson.lines.filter(item => item.name === 'The Polymath')[0];
   const divCardJson = divCardResponseJson.lines;
   const uniqueDivCards = divCardJson.map(function(item){
     if (item.explicitModifiers[0]) {
@@ -27,9 +78,7 @@ async function main(){
       return null
     }
   }).filter(Boolean);
-  //console.log('uniqueDivCards' , uniqueDivCards);
-  //https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueMap&language=en
-  //https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en
+  // console.log('uniqueDivCards' , uniqueDivCards);
   // fetch the unique Accessories
   const uniqueAccessories = (await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueAccessory&language=en').then(r => r.json())).lines;
   const uniqueMaps = (await fetch('https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueMap&language=en').then(r => r.json())).lines;
@@ -40,7 +89,7 @@ async function main(){
   const uniqueItems = [].concat(uniqueMaps, uniqueFlasks, uniqueJewels, uniqueAccessories, uniqueArmours, uniqueWeapons);
 
   // single out item
-  uniqueItems.forEach(function (uniqueItem) {
+  const cardAndItemDatas = uniqueItems.map(function (uniqueItem) {
     const combinedItem = {
       name: uniqueItem.name,
       stackSize: uniqueItem.stackSize,
@@ -48,12 +97,16 @@ async function main(){
     };
     const matchingUniqueDivCard = uniqueDivCards.find(divCard => divCard.uniqueItem === combinedItem.name);
     if (!matchingUniqueDivCard){
-      return;
+      return false;
     }
     // calculating profit margins
     const divCardStackPrice = matchingUniqueDivCard.stackSize * matchingUniqueDivCard.chaos;
     const profitMargin = combinedItem.chaos - divCardStackPrice;
-    console.log(`By flipping ${matchingUniqueDivCard.name} into ${combinedItem.name} you can make ${Math.round(profitMargin)} chaos.`);
-  });
+    const cardAndItemData = {matchingUniqueDivCard, combinedItem, divCardStackPrice, profitMargin};
+    return cardAndItemData;
+    // console.log(`By flipping ${matchingUniqueDivCard.name} into ${combinedItem.name} you can make ${Math.round(profitMargin)} chaos.`);
+  }).filter(Boolean);
+  return cardAndItemDatas;
 };
-main();
+module.exports = getDivCardsAndItems;
+// getDivCardsAndItems();
