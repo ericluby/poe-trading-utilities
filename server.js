29496fd32823f6d1864873214f8f3a6514a745ff
ericluby <60050306+ericluby@users.noreply.github.com>
2020-01-27 16:24:44 -0500

    html working

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const getDivCardsAndItems = require('./getDivCardsAndItems.js')
-const server = express()
-const port = 3000
-
+const express = require('express');
+var cors = require('cors');
+const getDivCardsAndItems = require('./getDivCardsAndItems.js');
+const server = express();
+const port = 3000;
+server.use(cors());
 // server.get('/', (req, res) => res.send('Hello World!'))
 // server.get('/foo', (req, res) => {
 //   res.json({a : 1})
 //   console.log('FooBar')
 // })
 server.get('/divitems', async function divItemsHandler (req, res) {
-  console.log('Starting to get the Div Items')
+  console.log('Starting to get the Div Items');
   const storedData = await getDivCardsAndItems();
-  res.json(storedData)
-  console.log('Successfully got the Div Items. Sending them back now.')
-})
+  res.json(storedData);
+  console.log('Successfully got the Div Items. Sending them back now.');
+});
 
-server.listen(port, () => console.log(`Server starting on port ${port}!`))
+server.listen(port, () => console.log(`Server starting on port ${port}!`));
