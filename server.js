997f5e3842853fa931c8f4beee7ee9779f4b4a98
ericluby <60050306+ericluby@users.noreply.github.com>
2020-02-07 18:52:29 -0500

    deploying

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
 var cors = require('cors');
 const getDivCardsAndItems = require('./getDivCardsAndItems.js');
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.use(cors());
-// server.get('/', (req, res) => res.send('Hello World!'))
+server.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
 // server.get('/foo', (req, res) => {
 //   res.json({a : 1})
 //   console.log('FooBar')
