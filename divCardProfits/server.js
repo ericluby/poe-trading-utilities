633a802a940dacdd3ff5d38d03cc5d8e8eb7fde2
ericluby <60050306+ericluby@users.noreply.github.com>
2020-02-19 19:12:07 -0500

    move divCardProfits into its old folder

--- divCardProfits/server.js
+++ divCardProfits/server.js
@@ -5,11 +5,8 @@ const getDivCardsAndItems = require('./getDivCardsAndItems.js');
 const server = express();
 const port = process.env.PORT || 3000;
 server.use(cors());
+//access website at http://localhost:3000/
 server.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
-// server.get('/foo', (req, res) => {
-//   res.json({a : 1})
-//   console.log('FooBar')
-// })
 server.get('/divitems', async function divItemsHandler (req, res) {
   console.log('Starting to get the Div Items');
   const storedData = await getDivCardsAndItems();
